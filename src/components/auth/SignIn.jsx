be2b4619533fa340008ca9a6206007442e8fe349
Emory <ro25ck@yahoo.com>
2024-06-24 09:51:39 +0100

    reconfigured TailwindCSS.config.js to recognize and render CSS code as expected

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,26 +1,42 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { logIn } from '../../firebase';
+import { logIn } from "../../firebase";
 
-export default function SignIn() { 
+export default function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("")
   const navigate = useNavigate();
 
   useEffect(() => {
     const lastPage = window.location.pathname;
-    if (lastPage !== '/sign-in') {
-      localStorage.setItem('lastPage', lastPage);
+    if (lastPage !== "/sign-in") {
+      localStorage.setItem("lastPage", lastPage);
     }
   }, []);
 
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
   const signIn = async (e) => {
     e.preventDefault();
+    if (email === "") {
+      setEmailError("Email address is required.");
+      return;
+    } else if (!validateEmail(email)) {
+      setEmailError("Please enter a valid email address.");
+      return;
+    } else {
+      setEmailError("");
+    }
+
     try {
       const userCredential = await logIn(email, password);
       console.log(userCredential, "sign-in page");
 
-      const lastPage = localStorage.getItem('lastPage');
+      const lastPage = localStorage.getItem("lastPage");
       navigate(lastPage ? lastPage : "/");
     } catch (error) {
       console.log(error);
@@ -29,27 +45,90 @@ export default function SignIn() {
 
   return (
     <>
-      <div className="sign-in--container">
-        <form onSubmit={signIn}>
-          <h2>Log In to Your ArtiQuest Account</h2>
-          <input
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type="submit">Log In</button>
-        </form>
-      </div>
-      <div>
-        If you do not have an account, register for one here{" "}
-        <Link to="/sign-up">register for new account</Link>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          {/* <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          /> */}
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+          {emailError && (
+                  <p className="mt-2 text-sm text-red-600">{emailError}</p>
+                )}
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  // autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  {/* <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </a> */}
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            or{" "}
+            <Link className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500" to="/sign-up">CREATE AN ACCOUNT</Link>
+          </p>
+        </div>
       </div>
     </>
   );
